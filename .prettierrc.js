5e7187f0280ca18615cf58164381a6d65f5d67c8
yikl1004 <yikl1004@gmail.com>
2022-02-13 18:36:07 +0900

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+const { FAILSAFE_SCHEMA } = require("js-yaml");
+
+module.exports = {
+    trailingComma: "all",
+    useTabs: true,
+    tabWidth: 4,
+    singleQuote: true,
+    semi: false,
+    printWidth: 80,
+    jsxSingleQuote: false,
+    embeddedLanguageFormatting: "auto",
+    bracketSpacing: true,
+    rrowParens: "always",
+}
